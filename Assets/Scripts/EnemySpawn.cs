36ba15b1824c7b1679f90ae39e93c208aab6e4c8
Dajungin <jmj66638@gmail.com>
2026-07-30 18:40:17 +0900

    EnemySpawn
    적을 마음껏 추가 가능하게 함

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -2,21 +2,23 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    // 적 스폰시간, 스테이지에 따른 몬스터 종류와 능력 
-    //계속 적 종류를 추가할 수 있게 하기
-    //스폰을 화면 밖에서 하기
-    //
-    public GameObject monster;
+    [Header("Enemy Prefabs")]
+    public GameObject[] monsters;   // 여러 종류의 몬스터
 
+    [Header("Spawn")]
     public float time = 2f;
 
     void Start()
     {
-        InvokeRepeating("Spawn", 1, time);
+        InvokeRepeating(nameof(Spawn), 1f, time);
     }
 
     void Spawn()
     {
+        // 등록된 몬스터가 없으면 생성하지 않음
+        if (monsters.Length == 0)
+            return;
+
         Vector2 pos;
 
         int side = Random.Range(0, 4);
@@ -40,6 +42,9 @@ public class EnemySpawn : MonoBehaviour
                 break;
         }
 
-        Instantiate(monster, pos, Quaternion.identity);
+        // 랜덤 몬스터 선택
+        int randomIndex = Random.Range(0, monsters.Length);
+
+        Instantiate(monsters[randomIndex], pos, Quaternion.identity);
     }
 }
